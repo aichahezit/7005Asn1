2d188adbb919ef62c40cb5f9a8ae8de51439d410
root <aichez18@hotmail.com>
2016-09-27 12:01:02 -0700

    Client-Server communication established

--- file_clnt.c
+++ file_clnt.c
@@ -20,3 +20,129 @@
 -- 	prompted for a command; get or receive file. The command is then sent to the server and the
 -- 	response(file) from the server is displayed.
 ---------------------------------------------------------------------------------------*/
+#include <stdio.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+#define SERVER_TCP_PORT		7005	// Default port
+#define BUFLEN			80  	// Buffer length
+#define GET			0 
+#define SEND			1 
+
+int main (int argc, char **argv)
+{
+	//initialize variables
+	int n, bytes_to_read;
+	int sd, port;
+	struct hostent	*hp;
+	struct sockaddr_in server;
+	char  *host, *bp, rbuf[BUFLEN], sbuf[BUFLEN], **pptr, *sptr;
+	char str[16];
+	int result;
+	
+	//get port from cmd args
+
+	//temp
+	switch(argc)
+	{
+		case 2:
+			host =	argv[1];	// Host name
+			port =	SERVER_TCP_PORT;
+		break;
+		case 3:
+			host =	argv[1];
+			port =	atoi(argv[2]);	// User specified port
+		break;
+		default:
+			fprintf(stderr, "Usage: %s host [port]\n", argv[0]);
+			exit(1);
+	}
+
+	//create socket
+	if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	{
+		perror("Cannot create socket");
+		exit(1);
+	}
+	bzero((char *)&server, sizeof(struct sockaddr_in));
+	server.sin_family = AF_INET;
+	server.sin_port = htons(port);
+	if ((hp = gethostbyname(host)) == NULL)
+	{
+		fprintf(stderr, "Unknown server address\n");
+		exit(1);
+	}
+	bcopy(hp->h_addr, (char *)&server.sin_addr, hp->h_length);
+
+	//connect to server
+	if (connect (sd, (struct sockaddr *)&server, sizeof(server)) == -1)
+	{
+		fprintf(stderr, "Can't connect to server\n");
+		perror("connect");
+		exit(1);
+	}
+	printf("Connected:    Server Name: %s\n", hp->h_name);
+	pptr = hp->h_addr_list;
+	printf("\t\tIP Address: %s\n", inet_ntop(hp->h_addrtype, *pptr, str, sizeof(str)));
+	printf("Enter User Command (get or send):\n");
+
+	// get user's text
+	fgets (sbuf, BUFLEN, stdin);
+
+	//transfer string command for use in switch case
+	if(strcmp(sbuf, "get"))	{result = 0;}
+	if(strcmp(sbuf, "send")){result = 1;}
+
+	//switch statement for get or receive command
+	switch(result){
+		case GET:
+			//testing, TODO: get filename from args, send file name to server
+			send (sd, sbuf, BUFLEN, 0);
+			printf("Getting file from server...\n");
+			break;
+
+		case SEND:
+			//testing, TODO: create file, send to server
+			send (sd, sbuf, BUFLEN, 0);
+			printf("Sending file to server...\n");
+			break;
+
+		default:
+			fprintf(stderr, "Incorrect command.\n", argv[0]);
+			exit(1);
+	}
+
+	//!!!temp for testing
+	// Transmit data through the socket
+	//send (sd, sbuf, BUFLEN, 0);
+
+	printf("Received command:\n");
+	bp = rbuf;
+	bytes_to_read = BUFLEN;
+
+	// client makes repeated calls to recv until no more data is expected to arrive.
+	n = 0;
+	while ((n = recv (sd, bp, bytes_to_read, 0)) < BUFLEN)
+	{
+		bp += n;
+		bytes_to_read -= n;
+	}
+	printf ("%s\n", rbuf);
+	fflush(stdout);
+	close (sd);
+	return (0);
+}
+
+
+
+
+
+
--- file_svr.c
+++ file_svr.c
@@ -15,6 +15,82 @@
 --
 --	NOTES:
 --	The program will accept TCP connections from client machines.
--- 	The program will recieve commands from the client socket and eihter send or 
+-- 	The program will recieve commands from the client socket and either send or 
 -- 	recieve a file back.
 ---------------------------------------------------------------------------------------*/
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define SERVER_TCP_PORT 7005	// Default port
+#define BUFLEN	80		//Buffer length
+
+
+int main (int argc, char **argv)
+{
+
+	//initialize variables
+	int	n, bytes_to_read;
+	int	sd, new_sd, client_len, port;
+	struct	sockaddr_in server, client;
+	char	*bp, buf[BUFLEN];
+
+	//get cmd args for port
+	port = atoi(argv[1]);
+
+	//create stream socket
+	if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	{
+		perror ("Can't create a socket");
+		exit(1);
+	}
+
+	//bind address to socket
+	bzero((char *)&server, sizeof(struct sockaddr_in));
+	server.sin_family = AF_INET;
+	server.sin_port = htons(port);
+	server.sin_addr.s_addr = htonl(INADDR_ANY); // Accept connections from any client
+
+	if (bind(sd, (struct sockaddr *)&server, sizeof(server)) == -1)
+	{
+		perror("Can't bind name to socket");
+		exit(1);
+	}	
+
+	//listen for connections
+	listen(sd, 3);
+
+	while(1){
+	
+		//initialize client
+		client_len= sizeof(client);
+		
+		//accept client
+		if ((new_sd = accept (sd, (struct sockaddr *)&client, &client_len)) == -1)
+			{
+				fprintf(stderr, "Can't accept client\n");
+				exit(1);
+			}
+		
+		//temp for testing, replace with confirmation of recieved user command
+		bp = buf;
+		bytes_to_read = BUFLEN;
+		while ((n = recv (new_sd, bp, bytes_to_read, 0)) < BUFLEN)
+		{
+			bp += n;
+			bytes_to_read -= n;
+		}
+		printf ("sending:%s\n", buf);
+
+		send (new_sd, buf, BUFLEN, 0);
+		close (new_sd);
+	}
+
+	close(sd);
+	return(0);	
+}
